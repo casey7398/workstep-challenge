7fece426cb214c190a402d8dbe95f71ad622779d
Casey McIntosh <casey7398@gmail.com>
2021-08-26 21:59:53 -0700

    update

--- src/components/ApplicantTable.js
+++ src/components/ApplicantTable.js
@@ -1,55 +1,91 @@
-import React, { useState } from "react";
-import { useSelector } from 'react-redux';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function ApplicantTable() {
-    
-    const [loading, setLoading] = useState(true);
-    const [applicants, setApplicants] = useState();
-    const [error, setError] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [applicants, setApplicants] = useState();
+  const [error, setError] = useState(0);
 
-    const them = useSelector((state) => state.applicants);
-    console.log(them);
-    
+  const RenderedApplicant = () => {
     return (
-        <>
-            {/* search input */}
-            <div className="row" style={{ justifyContent: 'center' }}>
-                <div className="search-wrapper"><img src='/magGlass.svg' style={{ height: 15 }} /> <input placeholder="Start typing to filter by name..." className="search-input" /></div>
-            </div>
-            <div className="row mt-2" style={{ alignItems: 'baseline', justifyContent: 'center' }}>
-                <div className="row">
-                    <div className="left-selector-wrapper">
-                        {/* placeholder for now */}
-                        <div className="selector-item active">
-                            All Candidates (4)
-                        </div>
-                        <div className="selector-item">
-                            Paperwork (1)
-                        </div>
-                    </div>
-                </div>
-                <div className="table-container">
-                    <table className="applicant-table">
-                        <thead>
-                            <tr>
-                                <th className="t-head">Candidate</th>
-                                <th className="t-head">Date Interviewed</th>
-                                <th className="t-head" />
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td className="t-item"> Casey McIntosh</td>
-                                <td className="t-item"> Thu, Apr 9 2021</td>
-                                <td className="t-item"></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </>
+      <>
+        {applicants && applicants.map((applicant) => {
+          return (
+            <>
+              <tr key={applicant.id}>
+                <td class="t-item"> {applicant.name}</td>
+                <td class="t-item"> {applicant.time_interview}</td>
+                <td class="t-item"></td>
+              </tr>
+            </>
+          );
+        })}
+      </>
     );
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://my-json-server.typicode.com/workstep/react-challenge-data/candidates`
+      )
+      .then((res) => {
+        console.log("res.data", res.data);
+        if (res.data) {
+          setApplicants(res.data);
+        } else {
+          setError(404);
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (error) {
+    return <div>Error {error}</div>;
+  }
+
+  return (
+    <>
+      {/* search input */}
+      <div className="row" style={{ justifyContent: "center" }}>
+        <div className="search-wrapper">
+          <img src="/magGlass.svg" style={{ height: 15 }} />{" "}
+          <input
+            placeholder="Start typing to filter by name..."
+            className="search-input"
+          />
+        </div>
+      </div>
+      <div
+        className="row mt-2"
+        style={{ alignItems: "baseline", justifyContent: "center" }}
+      >
+        <div className="row">
+          <div className="left-selector-wrapper">
+            {/* placeholder for now */}
+            <div className="selector-item active">All Candidates (4)</div>
+            <div className="selector-item">Paperwork (1)</div>
+          </div>
+        </div>
+        <div className="table-container">
+          <table class="applicant-table">
+            <thead>
+              <tr>
+                <th class="t-head">Candidate</th>
+                <th class="t-head">Date Interviewed</th>
+                <th class="t-head" />
+              </tr>
+            </thead>
+            <tbody>
+              <RenderedApplicant />
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ApplicantTable
-
+export default ApplicantTable;
